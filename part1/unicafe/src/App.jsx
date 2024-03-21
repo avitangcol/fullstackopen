06923a31d66f6ffff31b7d90f445715708f559fc
Allan Vitangcol <allanv14email@gmail.com>
2024-03-21 10:22:06 -0400

    Add basic feedback and statistics to unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,42 @@
 import { useState } from 'react'
-//import './App.css'
 
-function App() {
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handlNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+  
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
-      unicafe
+      <h1>Give Feedback</h1>
+      <Button key="Good" handleClick={handleGoodClick} text="Good" />
+      <Button key="Neutral" handleClick={handlNeutralClick} text="Neutral" />
+      <Button key="Bad" handleClick={handleBadClick} text="Bad" />
+
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }
