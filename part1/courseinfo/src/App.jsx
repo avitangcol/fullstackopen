d6206fad35d27835712ab395276419e9e0bb6bdc
Allan Vitangcol <allanv14email@gmail.com>
2024-03-21 08:55:28 -0400

    Consolidate course data into single object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,53 +1,56 @@
 const Header = ({course}) => {
   return (
-    <h1>{course}</h1>
-  )
+    <h1>{course.name}</h1>
+  );
 }
 
-const Content = ({parts}) => {
+const Content = ({course}) => {
+  let parts = course.parts;
   return (
     <div>{parts.map((a) => <Part key={a.name} part={a.name} exercise={a.exercises} />)}</div>
-  )
+  );
 }
 
 const Part = ({part, exercise}) => {
   return (
     <p key={part}>{part} {exercise}</p>
-  )
+  );
 }
 
-const Total = ({exercises}) => {
-  let total = exercises.reduce((acc,obj) => acc + obj.exercises, 0)
+const Total = ({course}) => {
+  let parts = course.parts;
+  let total = parts.reduce((acc,obj) => acc + obj.exercises, 0);
   return (
     <p>Number of exercises {total}</p>
-  )
+  );
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      <Header course = {course} />
-      <Content parts={parts} />
-      <Total exercises={parts} />
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
-  )
+  );
 }
 
 export default App
